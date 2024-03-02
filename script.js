d99fc7696d9f1348c5f6b26162108571bbb0b6bd
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-03-02 11:05:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function fibonacci(num) {
 // your code here
-	if(num == 1){
-		return 0;
-	}
-	if(num == 2){
-		return 1;
-	}
-	return fibonacci(num-1) + fibonacci(num-2)
+	var a = 1, b = 0, temp;
+
+  while (num >= 0){
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
+
+  return b;
 }
 
 module.exports = fibonacci;
