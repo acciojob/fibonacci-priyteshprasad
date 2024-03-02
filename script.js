87478e0703d6aed9e85d816f1e68f5ad42a9ed4c
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-03-02 11:25:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function fibonacci(num) {
 // your code here
 	var a = 1, b = 0, temp;
+	if(num == 1) return 0;
+	if(num == 2) return 1;
+	
 
-  while (num >= 0){
+  while (num > 2){
     temp = a;
     a = a + b;
     b = temp;
     num--;
   }
 
-  return b;
+  return a; 
 }
+// var n = prompt("Enter the number of elements");
+// alert(fibonacci(n)); 
 
 module.exports = fibonacci;
